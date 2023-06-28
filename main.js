9181a6b30d2609bd25d3073b61d5b170eb150411
Felipe Rodrigues <felipetpr@hotmail.com>
2023-06-27 21:45:02 -0300

    feat: add zip api and dynamic query

---
+++ main.js
@@ -0,0 +1,18 @@
+async function buscaEndereco(cep) {
+    try {    
+        const consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const consultaCEPConvertida = await consultaCEP.json();
+
+        if (consultaCEPConvertida.erro) {
+            throw Error("CEP não existente!");
+        }
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    }
+    catch (erro) {
+        console.log(erro);
+    }
+}
+
+const cep = document.getElementById("cep");
+cep.addEventListener("focusout", () => buscaEndereco(cep.value)); 
