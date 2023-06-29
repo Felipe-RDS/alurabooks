1898f2a68468973c1ac15011c0054bd6ab508a73
Felipe Rodrigues <felipetpr@hotmail.com>
2023-06-29 17:49:28 -0300

    refactor: add extract method

--- app/filtrarLivros.js
+++ app/filtrarLivros.js
@@ -7,9 +7,28 @@ function filtrarLivros() {
     const categoria = elementoBtn.value;
 
     let livrosFiltrados = categoria === "disponivel" ? 
-    livros.filter((livro) => livro.quantidade > 0) : 
-    livros.filter((livro) => livro.categoria === categoria);
-
+    filtrarPorDisponibilidade() : filtrarPorCategoria(categoria);
     exibirLivros(livrosFiltrados);
+
+    if (categoria === "disponivel") {
+        const valorTotal = calcularValorTotal(livrosFiltrados);
+        exibirLivrosDisponiveis();
+    }
     
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter((livro) => livro.categoria === categoria);
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter((livro) => livro.quantidade > 0);
+}
+
+function exibirLivrosDisponiveis() {
+    elementoLivrosDisponiveis.innerHTML = `
+        <div class="livros__disponiveis">
+            <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+        </div>
+    `;
 }
